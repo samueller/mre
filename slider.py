9c94a519f5c736757b7ebc246a83e9b12eb22adc
Scott Mueller <scott@cs.ucla.edu>
2023-02-26 16:15:56 -0800

    removed reference to mu in session state

--- slider.py
+++ slider.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-mu = st.slider('$\mu$', 0.0, 5.0, 2.5, step=0.1, key='mu')
+mu = st.slider('$\mu$', 0.0, 5.0, 2.5, step=0.1)
 var = st.slider('$\sigma^2$', 0.0, mu, min(mu, st.session_state.var) if 'var' in st.session_state else 1.0, step=0.1, key='var')
 st.write('$\mu$:', mu, '$\sigma^2$:', var)
