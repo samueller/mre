78cf771c7ffd759075c017bd29937bd1d246201f
Scott Mueller <scott@cs.ucla.edu>
2023-02-27 17:20:43 -0800

    fixed mu dragging issue

--- slider.py
+++ slider.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
 mu = st.slider('$\mu$', 0.0, 5.0, 2.5, step=0.1)
-var = st.slider('$\sigma^2$', 0.0, mu, min(mu, st.session_state.var) if 'var' in st.session_state else 1.0, step=0.1, key='var')
+if 'var' in st.session_state:
+    st.session_state.var = min(mu, st.session_state.var)
+var = st.slider('$\sigma^2$', 0.0, mu, 1.0, step=0.1, key='var')
+
+# var = st.slider('$\sigma^2$', 0.0, mu, min(mu, st.session_state.var) if 'var' in st.session_state else 1.0, step=0.1, key='var')
+
 st.write('$\mu$:', mu, '$\sigma^2$:', var)
